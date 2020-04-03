cbc5adb95dc5746713d3de3d3d3412ab0e75c1f4
alexander589 <slobashev8@gmail.com>
2020-04-03 19:55:56 +0300

    HomeWork

---
+++ src/Person.java
@@ -0,0 +1,30 @@
+public class Person {
+
+    public static void main(String[] args) {
+        Person artem = new Person("Artem", "imcool");
+        artem.displayPerson();
+        artem.account.displayAccount();
+    }
+
+    String name;
+    Account account;
+
+    Person (String name, String password){
+        this.name = name;
+        account = new Account(password);
+    }
+    public void displayPerson(){
+        System.out.println("Person Name:" + name + " " +"Password:" + account.password);
+    }
+
+    public class Account {
+        public String password;
+
+        Account (String pass){
+            this.password = pass;
+        }
+        public void displayAccount(){
+            System.out.println("Account Login:" + Person.this.name + " " +"Password:" + password);
+        }
+    }
+}
